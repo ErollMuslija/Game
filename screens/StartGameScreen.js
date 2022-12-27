f7bf11de2b01d15649807e1d859091fe3529fe40
Eroll Muslija <eroll.muslija1@gmail.com>
2022-12-27 15:57:57 +0100

    Initial game screens created

---
+++ screens/StartGameScreen.js
@@ -0,0 +1,40 @@
+import { TextInput, View, StyleSheet } from "react-native";
+import PrimaryButton from "../components/PrimaryButton"
+
+const StartGameScreen = () => {
+ return (
+    <View  style={StartGameScreenStyle.container}>
+        <TextInput style={StartGameScreenStyle.numberInput} maxLength={2} />
+        <PrimaryButton>Reset</PrimaryButton>
+        <PrimaryButton>Confirm</PrimaryButton>
+    </View>
+ )
+}
+
+export default StartGameScreen;
+
+const StartGameScreenStyle = StyleSheet.create({
+    container: {
+        marginTop: 100,
+        padding: 15,
+        marginHorizontal: 15,
+        backgroundColor: '#72063c',
+        borderRadius: 8,
+        elevation: 4, //-> Only Android
+        shadowColor: 'black',
+        shadowOffset: {width: 0 , height: 2},
+        shadowRadius: 6,
+        shadowOpacity: 0.5
+    },
+    numberInput: {
+        height: 50,
+        width: 50,
+        fontSize: 32,
+        borderBottomColor: '#ddb52f',
+        borderBottomWidth: 2,
+        color: '#ddb52f',
+        marginVertical: 8, 
+        fontWeight: 'bold',
+        textAlign: 'center'
+    }
+})
